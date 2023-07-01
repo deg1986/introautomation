2961d046acac7f2daec409ecf20000806ad3114b
Diego <diego@Air-de-Diego.fibertel.com.ar>
2023-07-01 11:07:21 -0300

    Agregado de separacion de paginas

--- features/login.js
+++ features/login.js
@@ -1,8 +1,10 @@
-import { Selector, t } from 'testcafe';
+import LoginPage from '../pages/loginPage'
 
+const loginPage = new LoginPage();
 const urlBase = 'https://demoqa.com/login';
 const username = 'demoautomation';
 const password = 'Demoautomation01!';
+
 
 
 fixture `Login Test`// declare the fixture
@@ -10,25 +12,12 @@ fixture `Login Test`// declare the fixture
     
 
 test('Check page of testing', async t => { //declare of test
-//declare all action and asserts of test
-var logo = Selector('img').withAttribute('src','/images/Toolsqa.jpg');
-var inputUserName = Selector('#userName');
-var inputPassword = Selector('input').withAttribute('type','password');
-var buttonLogin = Selector('#login');
 
-//declare asserts
-   await t 
-        .wait(2000)
-        .expect(logo.exists).ok('The logo is not ok')
-        .expect(inputUserName.exists).ok('The username input is not ok')
-        .expect(inputPassword.exists).ok('The username input is not ok')
-        .expect(buttonLogin.exists).ok('The username input is not ok')
-        .expect(buttonLogin.withText('Login').exists).ok('The text in button is not ok');
+//asserts for check the page
+   await loginPage.checkForm();
 
 //declare actions
-    await t
-        .typeText(inputUserName, username)
-        .typeText(inputPassword, password)
-        //.wait(4000)
-        .click(buttonLogin);
+    
+    await loginPage.completeLoginForm(username,password);
+    await loginPage.clickLoginButton();
 });
